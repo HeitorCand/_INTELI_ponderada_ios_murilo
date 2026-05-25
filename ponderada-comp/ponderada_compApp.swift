10c0cf6100891906ca80f698d71c14115e2cecc7
Heitor Candido <heitor.candido@sou.inteli.edu.br>
2026-05-25 10:08:16 -0300

    Initial Commit

---
+++ ponderada-comp/ponderada_compApp.swift
@@ -0,0 +1,17 @@
+//
+//  ponderada_compApp.swift
+//  ponderada-comp
+//
+//  Created by Heitor Candido on 25/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ponderada_compApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
